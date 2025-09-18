78ce3fa95695a7049beb75b8ae7b8dc4bfb49543
Alex Mendes <alexanderhmendes@gmail.com>
2025-09-18 18:07:22 +0100

    feat: add names to custom errors (#25)

--- src/errors.ts
+++ src/errors.ts
@@ -12,13 +12,26 @@ export class MachineError<E extends ErrorType> extends Error {
   constructor(data: E) {
     super(data.code);
     this.data = data;
+    this.name = 'MachineError';
   }
 }
 
 export class QuoteError extends MachineError<
   InputValidDryError | InputValidWalletError
-> {}
+> {
+  constructor(data: InputValidDryError | InputValidWalletError) {
+    super(data);
+    this.name = 'QuoteError';
+  }
+}
 
 export class TransferError extends MachineError<
   QuoteSuccessTransferError | QuoteSuccessDirectTransferError
-> {}
+> {
+  constructor(
+    data: QuoteSuccessTransferError | QuoteSuccessDirectTransferError,
+  ) {
+    super(data);
+    this.name = 'TransferError';
+  }
+}
