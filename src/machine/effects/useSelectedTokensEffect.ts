2ab92cb76552dd35c0b30a1a5b650927589c6697
danila <danilabmt@gmail.com>
2025-09-30 16:25:06 +0200

    fix: fix default intents token selection

--- src/machine/effects/useSelectedTokensEffect.ts
+++ src/machine/effects/useSelectedTokensEffect.ts
@@ -150,10 +150,10 @@ export const useSelectedTokensEffect = ({
     const timer = setTimeout(() => {
       if (!sourceToken.token) {
         // 1. Intents token if possible
-        if (!skipIntents) {
+        if (!skipIntents && highestIntentsToken) {
           fireEvent('tokenSelect', {
             variant: 'source',
-            token: getDefaultIntentsToken({ tokens }),
+            token: highestIntentsToken,
           });
           // 2. Wallet base token if intents not supported
         } else if (walletSupportedChains.length) {
