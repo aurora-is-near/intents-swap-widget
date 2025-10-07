4679bb8fa841732a19a8c9dfb1f18f25c579063c
Maksim Vashchuk <maksim.vashchuk@aurora.dev>
2025-10-07 22:59:07 +0200

    fix: add clearTimeout

--- src/features/TokenInput/hooks/useTokenInputBalance.ts
+++ src/features/TokenInput/hooks/useTokenInputBalance.ts
@@ -12,23 +12,27 @@ export const useTokenInputBalance = (token: Token | undefined) => {
   );
 
   useEffect(() => {
+    let timeoutId: NodeJS.Timeout | undefined;
+
     if (token) {
       const existingBalance = mergedBalance[getTokenBalanceKey(token)];
 
       if (!existingBalance) {
         // if no balance can be fetched, we set 0 after 2s to avoid infinite loading state
-        setTimeout(() => {
+        timeoutId = setTimeout(() => {
           setSourceTokenBalance(
             token ? (mergedBalance[getTokenBalanceKey(token)] ?? 0) : 0,
           );
         }, 2000);
-
-        return undefined;
       }
     }
 
-    return undefined;
-  }, [token]);
+    return () => {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+    };
+  }, [token, mergedBalance]);
 
   return sourceTokenBalance;
 };
