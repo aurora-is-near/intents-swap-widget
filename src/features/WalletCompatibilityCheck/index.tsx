9224163a197ff18bb660581b1b96e1111cbf6d6e
FroggyRocky <43971330+FroggyRocky@users.noreply.github.com>
2025-10-17 14:25:13 +0200

    Feat/wallet compatibility check (#55)
    * init near to intents transaction setup
    
    * fix: fix participate widget, fix deposit the same asset as the sale contract deposit addres
    
    * fix: lint fixes
    
    * fix: address PR comments
    
    * feat: wallet compatibility check
    
    * chore: address pr comments
    
    * chore: address pr comments
    
    * chore: change icon color
    
    * fix: cleanup
    
    * fix: block body scroll
    
    * feat: add near public key within the compatibility check
    
    * fix: fix rabbit issues
    
    * fix: add buffer import
    
    * fix: fix solana signer
    
    * chore: make sure to restrict only scroll on y axis
    
    ---------
    
    Co-authored-by: Maksim Vashchuk <maksim.vashchuk@aurora.dev>

--- src/features/WalletCompatibilityCheck/index.tsx
+++ src/features/WalletCompatibilityCheck/index.tsx
@@ -38,13 +38,13 @@ export function WalletCompatibilityCheck({ onMsg, providers }: Props) {
 
   useEffect(() => {
     if (isOpen) {
-      window.document.body.style.overflow = 'hidden';
+      window.document.body.style.overflowY = 'hidden';
     } else {
-      window.document.body.style.overflow = 'auto';
+      window.document.body.style.overflowY = 'auto';
     }
 
     return () => {
-      window.document.body.style.overflow = 'auto';
+      window.document.body.style.overflowY = 'auto';
     };
   }, [isOpen]);
 
