a06c63800724a6e858379ab76e2ce996b5fc8eb2
alexandermendes <alexanderhmendes@gmail.com>
2025-10-26 23:50:55 +0000

    build: try a more explicit createAppKit

--- demo/main.tsx
+++ demo/main.tsx
@@ -1,8 +1,8 @@
-import './appkit-config';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createAppKit } from './appkit-config';
 import { globalStyles } from './globalStyles';
 
 import '../src/tailwind.css';
@@ -23,6 +23,8 @@ if (!container) {
 }
 
 const root = createRoot(container);
+
+createAppKit();
 
 root.render(
   <StrictMode>
