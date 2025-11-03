c1df40285c3e495cb1cd09ed0c98dee8dd7de307
Maksim Vashchuk <maksim.vashchuk@aurora.dev>
2025-11-03 11:44:12 +0100

    fix: styles n skeletons

--- packages/intents-swap-widget/src/widgets/WidgetSwap/WidgetSwapSkeleton.tsx
+++ packages/intents-swap-widget/src/widgets/WidgetSwap/WidgetSwapSkeleton.tsx
@@ -1,12 +1,14 @@
 import { SkeletonBox } from '@/components/SkeletonBox';
 
 export const WidgetSwapSkeleton = () => (
-  <div className="gap-sw-lg relative flex flex-col">
+  <div className="w-full gap-sw-lg relative flex flex-col">
     <div className="gap-sw-lg relative flex flex-col">
-      <div className="gap-sw-lg relative flex flex-col">
-        <SkeletonBox className="h-[72px]" />
-        <SkeletonBox className="h-[72px]" />
-      </div>
+      <SkeletonBox className="h-[108px]" />
+      <SkeletonBox className="h-[108px]" />
+    </div>
+
+    <div className="gap-sw-lg relative flex flex-col">
+      <SkeletonBox className="h-[44px]" />
       <SkeletonBox className="h-[48px]" />
     </div>
   </div>
--- packages/intents-swap-widget/src/widgets/WidgetWithdraw/WidgetWithdrawSkeleton.tsx
+++ packages/intents-swap-widget/src/widgets/WidgetWithdraw/WidgetWithdrawSkeleton.tsx
@@ -1,9 +1,16 @@
 import { SkeletonBox } from '@/components/SkeletonBox';
 
 export const WidgetWithdrawSkeleton = () => (
-  <div className="gap-sw-lg relative flex flex-col">
-    <SkeletonBox className="h-[120px]" />
-    <SkeletonBox className="h-[120px]" />
-    <SkeletonBox className="h-[48px]" />
+  <div className="w-full gap-sw-xl relative flex flex-col">
+    <div className="gap-sw-lg relative flex flex-col">
+      <SkeletonBox className="h-[157px]" />
+      <SkeletonBox className="h-[157px]" />
+      <SkeletonBox className="h-[152px]" />
+    </div>
+
+    <div className="gap-sw-lg relative flex flex-col">
+      <SkeletonBox className="h-[120px]" />
+      <SkeletonBox className="h-[48px]" />
+    </div>
   </div>
 );
