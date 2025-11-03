c94ad25d155ebe431831c8df1d728d708636adb6
alexandermendes <alexanderhmendes@gmail.com>
2025-11-03 08:36:30 +0000

    fix: swt loading skeletons to full width

--- packages/intents-swap-widget/src/widgets/WidgetSwap/WidgetSwapSkeleton.tsx
+++ packages/intents-swap-widget/src/widgets/WidgetSwap/WidgetSwapSkeleton.tsx
@@ -1,9 +1,9 @@
 import { SkeletonBox } from '@/components/SkeletonBox';
 
 export const WidgetSwapSkeleton = () => (
-  <div className="gap-sw-lg relative flex flex-col">
-    <div className="gap-sw-lg relative flex flex-col">
-      <div className="gap-sw-lg relative flex flex-col">
+  <div className="gap-sw-lg relative flex flex-col w-full">
+    <div className="gap-sw-lg relative flex flex-col w-full">
+      <div className="gap-sw-lg relative flex flex-col w-full">
         <SkeletonBox className="h-[72px]" />
         <SkeletonBox className="h-[72px]" />
       </div>
