6fc118a41eaf859a9691a971f8a180cdeac18e4e
alexandermendes <alexanderhmendes@gmail.com>
2025-11-07 14:19:26 +0000

    fix: update memelandia prod url

--- apps/ton-memelandia/src/appkit-config.tsx
+++ apps/ton-memelandia/src/appkit-config.tsx
@@ -17,7 +17,7 @@ import {
 } from '@solana/wallet-adapter-wallets';
 
 const projectId = '76f61d4322c80976d1a24a1263a9d082';
-const appUrl = 'https://ton-memelandia-widget.vercel.app';
+const appUrl = 'https://www.ton-intents.com';
 
 const metadata = {
   name: 'Intents Swap Widget',
