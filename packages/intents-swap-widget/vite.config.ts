b36c2a10728d09c56105328a295bb6755a99dfd8
Maksim Vashchuk <maksim.vashchuk@aurora.dev>
2025-11-23 14:55:47 +0100

    fix: lint

--- packages/intents-swap-widget/vite.config.ts
+++ packages/intents-swap-widget/vite.config.ts
@@ -1,4 +1,3 @@
-// @ts-expect-error no glob package types
 import { glob } from 'glob';
 import { fileURLToPath } from 'node:url';
 import { dirname, resolve, relative, extname } from 'node:path';
@@ -91,7 +90,6 @@ export default defineConfig({
       },
       input: Object.fromEntries(
         // https://rollupjs.org/configuration-options/#input
-        // @ts-expect-error file has type any
         glob.sync('src/**/*.{ts,tsx}').map((file) => [
           // 1. The name of the entry point
           // lib/nested/foo.js becomes nested/foo
