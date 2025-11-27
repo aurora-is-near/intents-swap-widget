b37c279e3bc8299efabc31692f543bf9597554ec
Maksim Vashchuk <maksim.vashchuk@aurora.dev>
2025-11-27 17:49:44 +0100

    fix: add alchemy key env

--- apps/demo/src/TabbedWidgetsDemo.tsx
+++ apps/demo/src/TabbedWidgetsDemo.tsx
@@ -53,6 +53,7 @@ export const TabbedWidgetsDemo = () => {
         config={{
           appName: 'Demo App',
           connectedWallets: { default: walletAddress },
+          alchemyApiKey: process.env.ALCHEMY_API_KEY,
           intentsAccountType: 'near',
           chainsFilter: {
             target: {
