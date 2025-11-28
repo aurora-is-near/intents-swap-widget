1a7b3be78b7634c3b3df7c68e25d99e978cf1eab
danila <danilabmt@gmail.com>
2025-11-28 10:48:40 +0100

    chore: add vercel plugin

--- apps/widget-creator/vite.config.ts
+++ apps/widget-creator/vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import { Plugin } from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import vercel from 'vite-plugin-vercel';
 
 export default defineConfig({
-  plugins: [react(), svgr(), nodePolyfills()] as Plugin[],
+  plugins: [react(), svgr(), nodePolyfills(), vercel()] as Plugin[],
 });
