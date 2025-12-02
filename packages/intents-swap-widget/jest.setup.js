b6c2cd7411a1481cf16a1bb54c4cff532992cb83
Maksim Vashchuk <maksim.vashchuk@aurora.dev>
2025-12-02 14:25:47 +0100

    fix: remove cryptr

--- packages/intents-swap-widget/jest.setup.js
+++ packages/intents-swap-widget/jest.setup.js
@@ -2,14 +2,6 @@ const {
   headlessUITransitionMock,
 } = require('./src/tests/mock-headless-ui-transition');
 
-jest.mock('cryptr', () => ({
-  __esModule: true,
-  default: jest.fn(() => ({
-    encrypt: jest.fn(() => 'encrypted-text'),
-    decrypt: jest.fn(() => 'decrypted-text'),
-  })),
-}));
-
 jest.mock('@headlessui/react', () => {
   return headlessUITransitionMock(jest.requireActual('@headlessui/react'));
 });
