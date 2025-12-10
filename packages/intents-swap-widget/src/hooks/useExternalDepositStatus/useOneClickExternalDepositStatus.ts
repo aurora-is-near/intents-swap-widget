64a1e5c37c5a43f33020ceb24e8dbce2bbc7ce09
Maksim Vashchuk <maksim.vashchuk@aurora.dev>
2025-12-10 15:48:07 +0100

    fix: remove mocked code

--- packages/intents-swap-widget/src/hooks/useExternalDepositStatus/useOneClickExternalDepositStatus.ts
+++ packages/intents-swap-widget/src/hooks/useExternalDepositStatus/useOneClickExternalDepositStatus.ts
@@ -1,45 +1,11 @@
-import {
-  GetExecutionStatusResponse,
-  OneClickService,
-} from '@defuse-protocol/one-click-sdk-typescript';
+import { OneClickService } from '@defuse-protocol/one-click-sdk-typescript';
 
 type Args = {
   depositAddress: string;
 };
 
-let counter = 0;
-
 export const useOneClickExternalDepositStatus = ({ depositAddress }: Args) => {
   const pollDepositStatus = async () => {
-    counter += 1;
-
-    if (counter > 3) {
-      return {
-        updatedAt: '2025-12-11T13:05:42.924Z',
-        status: GetExecutionStatusResponse.status.INCOMPLETE_DEPOSIT,
-        swapDetails: {
-          intentHashes: [],
-          nearTxHashes: [],
-          originChainTxHashes: [],
-          destinationChainTxHashes: [],
-        },
-        quoteResponse: {
-          amountIn: '100000',
-          amountInFormatted: '0.001',
-          amountInUsd: '0.0842',
-          minAmountIn: '100000',
-          amountOut: '84255',
-          amountOutFormatted: '0.084255',
-          amountOutUsd: '0.0842',
-          minAmountOut: '83412',
-          timeEstimate: 125,
-          deadline: '2025-12-11T13:05:42.924Z',
-          timeWhenInactive: '2025-12-11T13:05:42.924Z',
-          depositAddress: 'LcgEc9pNJSXkfkYFPNe6jQHs2yAg53qfKs',
-        },
-      };
-    }
-
     return OneClickService.getExecutionStatus(depositAddress);
   };
 
