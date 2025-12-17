033bcc055042c2dd4bd9d871d5252390098f5dc5
alexandermendes <alexanderhmendes@gmail.com>
2025-12-17 19:46:45 +0000

    refactor: replace base64 files with svgs

--- packages/intents-swap-widget/src/icons/index.tsx
+++ packages/intents-swap-widget/src/icons/index.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable import/extensions */
+import AbgIcon from './abg.svg?react';
 import ArbIcon from './arb.svg?react';
 import AsterIcon from './aster.svg?react';
 import AuroraIcon from './aurora.svg?react';
@@ -14,32 +15,30 @@ import FraxIcon from './frax.svg?react';
 import GnearIcon from './gnear.svg?react';
 import GnosisIcon from './gnosis.svg?react';
 import ItlxIcon from './itlx.svg?react';
+import JamboIcon from './jambo.svg?react';
 import KncIcon from './knc.svg?react';
 import LoudIcon from './loud.svg?react';
 import LtcIcon from './ltc.svg?react';
 import MonIcon from './mon.svg?react';
 import MpdaoIcon from './mpdao.svg?react';
 import NearIcon from './near.svg?react';
+import NoearIcon from './noear.svg?react';
 import OpIcon from './op.svg?react';
 import PolIcon from './pol.svg?react';
 import PublicIcon from './public.svg?react';
+import PurgeIcon from './purge.svg?react';
+import RheaIcon from './rhea.svg?react';
 import SolIcon from './sol.svg?react';
 import SuiIcon from './sui.svg?react';
 import TonIcon from './ton.svg?react';
 import TronIcon from './tron.svg?react';
 import UnknownIcon from './unknown.svg?react';
+import Usd1Icon from './usd1.svg?react';
+import UsdfIcon from './usdf.svg?react';
+import WifIcon from './wif.svg?react';
 import XdaiIcon from './xdai.svg?react';
 import XrpIcon from './xrp.svg?react';
 import ZecIcon from './zec.svg?react';
-
-import AbgIcon from './abg.png.base64?raw';
-import JamboIcon from './jambo.png.base64?raw';
-import NoearIcon from './noear.png.base64?raw';
-import PurgeIcon from './purge.png.base64?raw';
-import RheaIcon from './rhea.png.base64?raw';
-import Usd1Icon from './usd1.png.base64?raw';
-import UsdfIcon from './usdf.png.base64?raw';
-import WifIcon from './wif.png.base64?raw';
 
 import type { Chains } from '@/types/chain';
 
@@ -75,6 +74,7 @@ export const CHAIN_ICONS: Record<Chains, React.ReactElement> = {
 };
 
 export const ASSET_ICONS: Record<string, React.ReactElement> = {
+  abg: <AbgIcon />,
   arb: <ArbIcon />,
   aster: <AsterIcon />,
   aurora: <AuroraIcon />,
@@ -90,31 +90,28 @@ export const ASSET_ICONS: Record<string, React.ReactElement> = {
   gnear: <GnearIcon />,
   gnosis: <GnosisIcon />,
   itlx: <ItlxIcon />,
+  jambo: <JamboIcon />,
   knc: <KncIcon />,
   loud: <LoudIcon />,
   ltc: <LtcIcon />,
   mon: <MonIcon />,
   mpdao: <MpdaoIcon />,
   near: <NearIcon />,
+  noear: <NoearIcon />,
   op: <OpIcon />,
   pol: <PolIcon />,
   public: <PublicIcon />,
+  purge: <PurgeIcon />,
+  rhea: <RheaIcon />,
   sol: <SolIcon />,
   sui: <SuiIcon />,
   ton: <TonIcon />,
   tron: <TronIcon />,
+  usd1: <Usd1Icon />,
+  usdf: <UsdfIcon />,
   unknown: <UnknownIcon />,
+  wif: <WifIcon />,
   xdai: <XdaiIcon />,
   xrp: <XrpIcon />,
   zec: <ZecIcon />,
-
-  // base64 PNG icons
-  abg: <img src={AbgIcon} alt="ABG" />,
-  jambo: <img src={JamboIcon} alt="JAMBO" />,
-  noear: <img src={NoearIcon} alt="NOEAR" />,
-  purge: <img src={PurgeIcon} alt="PURGE" />,
-  rhea: <img src={RheaIcon} alt="RHEA" />,
-  usd1: <img src={Usd1Icon} alt="USD1" />,
-  usdf: <img src={UsdfIcon} alt="USDF" />,
-  wif: <img src={WifIcon} alt="WIF" />,
 };
