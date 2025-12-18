555f24c1b7ac8c1bfdc1901b41a321b70bcb239e
Vashchuk Maksim <maksim.vashchuk@aurora.dev>
2025-12-18 13:09:43 +0100

    fix: ref error and accessibility (#130)
    * fix: ref error and keys nav
    
    * fix: esc to close modals
    
    * fix: add key to effect deps
    
    * fix: remove browser check from use effect

--- packages/intents-swap-widget/src/features/ChainsDropdown/ChainItem.tsx
+++ packages/intents-swap-widget/src/features/ChainsDropdown/ChainItem.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import * as Icons from 'lucide-react';
 
 import { cn } from '@/utils/cn';
@@ -8,25 +9,32 @@ type Msg = { type: 'on_click_chain'; chain: 'intents' | 'all' | Chains };
 
 type Props = {
   label: string;
+  isFocused: boolean;
   isSelected: boolean;
   chain: 'intents' | 'all' | Chains;
   icon: string | React.ReactElement;
+  className?: string;
   onMsg: (msg: Msg) => void;
 };
 
-export const ChainItem = ({ chain, label, icon, isSelected, onMsg }: Props) => {
-  return (
+export const ChainItem = React.forwardRef<HTMLButtonElement, Props>(
+  ({ chain, label, icon, isFocused, isSelected, className, onMsg }, ref) => (
     <button
+      ref={ref}
       type="button"
       onClick={() => onMsg({ type: 'on_click_chain', chain })}
       className={cn(
         'gap-sw-md px-sw-lg py-sw-sm text-sw-gray-50 flex cursor-pointer items-center justify-between rounded-sw-md transition-colors',
-        { 'hover:text-sw-accent-200 hover:bg-sw-gray-700': !isSelected },
-        { 'bg-sw-gray-700': isSelected },
+        { 'bg-sw-gray-700': isSelected || isFocused },
+        {
+          'hover:text-sw-accent-200 hover:bg-sw-gray-700':
+            !isSelected && !isFocused,
+        },
+        className,
       )}>
       <Icon radius={10} label={label} icon={icon} />
       <span className="text-sw-label-md mr-auto">{label}</span>
       {isSelected && <Icons.Check size={16} />}
     </button>
-  );
-};
+  ),
+);
