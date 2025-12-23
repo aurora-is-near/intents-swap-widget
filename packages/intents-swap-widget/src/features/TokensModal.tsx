88ca79da0c83b2f46176861ea4b7837a24ac4a48
Maksim Vashchuk <maksim.vashchuk@aurora.dev>
2025-12-23 21:56:09 +0100

    fix: search keyboard navigation

--- packages/intents-swap-widget/src/features/TokensModal.tsx
+++ packages/intents-swap-widget/src/features/TokensModal.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import * as Icons from 'lucide-react';
 
 import { TokensList } from './TokensList';
@@ -46,12 +46,28 @@ export const TokensModal = ({
   const { ctx } = useUnsafeSnapshot();
   const { walletSupportedChains } = useConfig();
 
+  const searchInputRef = useRef<HTMLInputElement>(null);
   const [search, setSearch] = useState('');
   const chains = useChains(variant);
 
   const handleClose = () => onMsg({ type: 'on_dismiss_tokens_modal' });
 
-  useHandleKeyDown('Escape', handleClose);
+  useHandleKeyDown(
+    'Escape',
+    () => {
+      if (search) {
+        setSearch('');
+      } else {
+        handleClose();
+      }
+    },
+    [search],
+  );
+
+  useHandleKeyDown('Alphanumeric', (key) => {
+    setSearch((s) => s + key);
+    searchInputRef.current?.focus();
+  });
 
   // If there is only one chain available, select it by default
   const defaultChain =
@@ -84,6 +100,7 @@ export const TokensModal = ({
         <Input
           focusOnMount
           icon={Icons.Search}
+          ref={searchInputRef}
           defaultValue={search}
           className="w-full"
           placeholder="Search or paste address"
