a11df8db07b410cd7e0955ffd3a35d218304d327
alexandermendes <alexanderhmendes@gmail.com>
2026-01-06 22:43:25 +0000

    fix: update chains

--- packages/intents-swap-widget/src/chains.ts
+++ packages/intents-swap-widget/src/chains.ts
@@ -1,8 +1,14 @@
+import { cloneElement } from 'react';
 import { CHAINS_LIST } from './constants';
 import { CHAIN_ICONS } from './icons';
 
-export const CHAINS = Object.values(CHAINS_LIST).map((chain) => ({
-  id: chain.id,
-  label: chain.label,
-  icon: CHAIN_ICONS[chain.id],
-}));
+export const CHAINS = Object.values(CHAINS_LIST).map((chain) => {
+  return {
+    id: chain.id,
+    label: chain.label,
+    icon: cloneElement(CHAIN_ICONS[chain.id], {
+      width: '100%',
+      height: '100%',
+    }),
+  };
+});
