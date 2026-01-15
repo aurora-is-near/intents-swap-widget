dcf28f67cbd7b8ddd1950ece106ff9d9a71cac83
alexandermendes <alexanderhmendes@gmail.com>
2026-01-15 07:32:35 +0000

    feat: make creator sidebar scrollable

--- apps/widget-creator/src/App.tsx
+++ apps/widget-creator/src/App.tsx
@@ -8,15 +8,15 @@ function App() {
   return (
     <QueryProvider>
       <CreatorProvider>
-        <main className="flex flex-col gap-4 p-4 h-full p-csw-2xl relative">
+        <main className="flex flex-col gap-4 p-4 h-full p-csw-2xl relative md:max-h-screen md:overflow-hidden">
           <Header />
-          <div className="flex-1 flex items-center md:items-stretch flex-grow mt-csw-2xl gap-csw-2xl flex-col md:flex-row">
+          <div className="flex items-center md:items-stretch flex-grow mt-csw-2xl gap-csw-2xl flex-col md:flex-row md:h-[calc(100%-62px)]">
             <WidgetSection>
               <aside className="mt-csw-10xl m-auto sw">
                 <WidgetContent />
               </aside>
             </WidgetSection>
-            <section className="flex-grow bg-csw-gray-950 rounded-csw-lg max-w-full w-full md:max-w-[455px] md:w-full">
+            <section className="flex-grow bg-csw-gray-950 rounded-csw-lg max-w-full w-full md:max-w-[455px] md:w-full overflow-y-auto md:h-full">
               <CreatorPanel />
             </section>
           </div>
