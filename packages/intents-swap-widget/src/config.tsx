30241623aebe1b347c4b6d0d724fc8b8a1e8e858
alexandermendes <alexanderhmendes@gmail.com>
2026-01-17 22:13:53 +0000

    fix: restructure localisation to avoid setting state during render

--- packages/intents-swap-widget/src/config.tsx
+++ packages/intents-swap-widget/src/config.tsx
@@ -7,7 +7,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { I18nextProvider } from 'react-i18next';
 import type { PropsWithChildren } from 'react';
 
-import { initLocalisation } from './localisation';
 import { LocalisationDict } from './types/localisation';
 import { WidgetConfig } from './types/config';
 import { BalanceRpcLoader } from './features';
@@ -15,6 +14,7 @@ import { DEFAULT_RPCS } from './rpcs';
 import { ChainRpcUrls } from './types';
 import { Theme } from './types/theme';
 import { ThemeProvider } from './theme/ThemeProvider';
+import { useLocalisation } from './localisation';
 import { useAddClassToPortal } from '@/hooks/useAddClassToPortal';
 import { ErrorBoundary } from '@/features/ErrorBoundary';
 import { DEFAULT_CHAINS_ORDER, EVM_CHAINS } from '@/constants/chains';
@@ -122,7 +122,7 @@ export const WidgetConfigProvider = ({
   }, [userConfig]);
 
   // Initialise localisation
-  initLocalisation(localisation ?? {});
+  useLocalisation(localisation);
 
   // Add tailwind parent class to portal root
   useAddClassToPortal('headlessui-portal-root', 'sw');
