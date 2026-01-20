33d73b4db5af7d3329282aa8dfaecd7444c18622
Maksim Vashchuk <maksim.vashchuk@aurora.dev>
2026-01-20 11:47:32 +0100

    fix: remove unused var

--- packages/intents-swap-widget/src/widgets/WidgetDeposit/WidgetDepositContent.tsx
+++ packages/intents-swap-widget/src/widgets/WidgetDeposit/WidgetDepositContent.tsx
@@ -18,7 +18,7 @@ import { Banner, BlockingError } from '@/components';
 import { WalletCompatibilityCheck } from '@/features/WalletCompatibilityCheck';
 
 import { useStoreSideEffects } from '@/machine/effects';
-import { useComputedSnapshot, useUnsafeSnapshot } from '@/machine/snap';
+import { useUnsafeSnapshot } from '@/machine/snap';
 import { fireEvent } from '@/machine/events/utils/fireEvent';
 
 import {
@@ -47,7 +47,6 @@ export const WidgetDepositContent = ({
 }: Props) => {
   const { ctx } = useUnsafeSnapshot();
   const { t } = useTypedTranslation();
-  const { isDirectNearTokenWithdrawal } = useComputedSnapshot();
   const {
     chainsFilter: customChainsFilter,
     alchemyApiKey,
