9497e53047202453de8e9553b9bfd781ca6c4ac1
alexandermendes <alexanderhmendes@gmail.com>
2026-01-21 23:02:49 +0000

    feat: update select a token text height

--- apps/widget-creator/src/components/creatorPanel/SelectATokenText.tsx
+++ apps/widget-creator/src/components/creatorPanel/SelectATokenText.tsx
@@ -1,5 +1,10 @@
 export const SelectATokenText = () => (
-  <span className="flex items-center justify-center gap-csw-sm px-csw-md py-csw-sm rounded-csw-md bg-csw-gray-800 font-medium text-sm leading-4 text-csw-gray-50 tracking-[-0.4px] whitespace-nowrap shrink-0">
+  <span className="flex items-center justify-center px-csw-md py-csw-sm rounded-csw-md bg-csw-gray-800 font-medium text-sm leading-4 text-csw-gray-50 tracking-[-0.4px] whitespace-nowrap shrink-0 select-none">
+    {/*
+      The 28px height spacer is to match the size of our token icons
+      and make the box the same height
+    */}
+    <span className="h-[28px]" />
     Select a token
   </span>
 );
