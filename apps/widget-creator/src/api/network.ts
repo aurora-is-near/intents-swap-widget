cf333bba7d8cd81a72485fb237fe5c0548daffc2
Maksim Vashchuk <maksim.vashchuk@aurora.dev>
2026-01-30 13:08:16 +0100

    fix: remove localhost

--- apps/widget-creator/src/api/network.ts
+++ apps/widget-creator/src/api/network.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api',
-  // baseURL: 'https://intents-api.aurora.dev/api',
+  //   baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://intents-api.aurora.dev/api',
   headers: {
     'Content-Type': 'application/json',
   },
