e608ebbea068c72a81e80be1f12efb301c9527a9
Maksim Vashchuk <maksim.vashchuk@aurora.dev>
2026-02-03 13:56:16 +0100

    fix: add powered by aurora

--- apps/widget-creator/src/components/widget/WidgetSection.tsx
+++ apps/widget-creator/src/components/widget/WidgetSection.tsx
@@ -2,6 +2,7 @@ import { PropsWithChildren, useEffect, useState } from 'react';
 import { useCreator } from '../../hooks/useCreatorConfig';
 import { DEFAULT_BACKGROUND_COLOR } from '../../constants';
 import { getWidgetColor } from '../../utils/get-widget-color';
+import AuroraIcon from '../../assets/icons/aurora.svg?react';
 
 export const WidgetSection = ({ children }: PropsWithChildren) => {
   const { state } = useCreator();
@@ -29,9 +30,18 @@ export const WidgetSection = ({ children }: PropsWithChildren) => {
 
   return (
     <section
-      className="flex-grow rounded-csw-lg px-csw-2xl pb-csw-4xl max-w-full md:max-w-none w-full overflow-y-auto custom-scrollbar custom-scrollbar-offset-2xl"
+      className="relative flex-grow rounded-csw-lg px-csw-2xl pb-csw-4xl max-w-full md:max-w-none w-full overflow-y-auto custom-scrollbar custom-scrollbar-offset-2xl"
       style={{ backgroundColor }}>
       {children}
+
+      <div
+        className="absolute w-full flex items-center justify-center gap-csw-2md text-center text-csw-label-md text-csw-gray-300 p-csw-2xl"
+        // for some reason tailwind classes are not working here
+        style={{ bottom: 0, left: 0 }}>
+        Powered by
+        <AuroraIcon className="w-[18px] h-[18px] text-csw-gray-300" />
+        Aurora Labs
+      </div>
     </section>
   );
 };
