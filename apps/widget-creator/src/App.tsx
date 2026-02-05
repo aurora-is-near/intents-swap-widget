dea1ae186ace1237b3e3dec7670a4668c21b8000
alexandermendes <alexanderhmendes@gmail.com>
2026-02-05 22:47:01 +0000

    fix: tweak mobile styles

--- apps/widget-creator/src/App.tsx
+++ apps/widget-creator/src/App.tsx
@@ -8,11 +8,11 @@ function App() {
   return (
     <QueryProvider>
       <CreatorProvider>
-        <main className="flex flex-col p-4 h-full p-csw-2xl relative md:max-h-screen md:overflow-hidden bg-[#1D1E24]">
+        <main className="flex flex-col py-csw-2xl sm:p-csw-2xl relative sm:h-full md:max-h-screen md:overflow-hidden bg-[#1D1E24]">
           <Header />
           <div className="flex items-center md:items-stretch flex-grow mt-csw-2xl gap-csw-2xl flex-col md:flex-row md:h-[calc(100%-62px)]">
             <WidgetSection>
-              <aside className="mt-csw-10xl m-auto sw">
+              <aside className="mt-csw-2xl sm:mt-csw-10xl m-auto sw">
                 <WidgetContent />
               </aside>
             </WidgetSection>
