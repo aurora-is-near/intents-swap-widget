942f7f8569a26cb79e7ea383fdc60c9d37257266
Maksim Vashchuk <maksim.vashchuk@aurora.dev>
2026-02-09 12:59:51 +0100

    fix: rename app key in schema

--- apps/widget-creator/src/api/schemas.ts
+++ apps/widget-creator/src/api/schemas.ts
@@ -53,14 +53,9 @@ export const FeeConfigSchema = z.any().superRefine((data, ctx) => {
   }
 }) as z.ZodType<FeeConfig>;
 
-export const apiKeySchema: z.ZodSchema<ApiKey> = z
-  .object({
-    isEnabled: z.boolean(),
-    createdAt: z.string(),
-    widgetAppKey: z.string(),
-    feeRules: FeeConfigSchema,
-  })
-  .transform((data) => ({
-    ...data,
-    widgetApiKey: data.widgetAppKey,
-  }));
+export const apiKeySchema: z.ZodSchema<ApiKey> = z.object({
+  isEnabled: z.boolean(),
+  createdAt: z.string(),
+  widgetApiKey: z.string(),
+  feeRules: FeeConfigSchema,
+});
