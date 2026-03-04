d85ef736780f757489984f12c31404b5029ebbfa
alexandermendes <alexanderhmendes@gmail.com>
2026-03-04 20:45:09 +0000

    feat: add new intents studio logo

--- apps/widget-creator/src/components/Header.tsx
+++ apps/widget-creator/src/components/Header.tsx
@@ -39,10 +39,14 @@ export function Header({ onOpenDrawer, onOpenExportModal }: HeaderProps) {
   return (
     <header className="w-full flex items-center justify-between px-csw-2xl sm:px-csw-auto">
       <div className="gap-csw-2xl items-center flex">
-        <span className="font-medium text-xl tracking-[-0.5px] text-csw-gray-100 flex flex-row whitespace-nowrap">
-          <AuroraIcon className="w-[25px] h-[25px] text-csw-gray-100 mr-csw-lg sm:mr-csw-xl" />
-          Intents Widget
-        </span>
+        <div className="font-medium text-xl tracking-[-0.5px] text-csw-gray-100 flex flex-row items-center whitespace-nowrap">
+          <img
+            src="/images/near-intents-logo.png"
+            alt="Intents Logo"
+            className="w-[114px] h-auto relative bottom-[0.5px]"
+          />
+          Widget Studio
+        </div>
         <HeaderButton
           TrailingIcon={ExternalLink}
           href="https://docs.intents.aurora.dev/getting-started"
