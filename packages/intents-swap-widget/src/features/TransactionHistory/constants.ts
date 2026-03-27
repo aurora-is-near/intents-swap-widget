fe4833d8c086157fc787f03aeb640adba34834a7
Maksim Vashchuk <maksim.vashchuk@aurora.dev>
2026-03-27 13:20:25 +0100

    fix: add padding to tx item height

--- packages/intents-swap-widget/src/features/TransactionHistory/constants.ts
+++ packages/intents-swap-widget/src/features/TransactionHistory/constants.ts
@@ -1,4 +1,4 @@
-export const TX_ITEM_HEIGHT = 88;
+export const TX_ITEM_HEIGHT = 100;
 export const MIN_LIST_VIEW_AREA_HEIGHT = 200;
 export const MAX_LIST_VIEW_AREA_HEIGHT = 800;
 export const LIST_CONTAINER_ID = 'virtual-transactions-list';
