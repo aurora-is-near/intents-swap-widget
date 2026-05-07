f896da4f2d6233e37e4c4753129e5d91c87174e6
alexandermendes <alexanderhmendes@gmail.com>
2026-05-07 21:08:54 +0100

    feat: add hover colour to wallet select modal

--- packages/intents-swap-widget-standalone/src/WalletOptionCard.tsx
+++ packages/intents-swap-widget-standalone/src/WalletOptionCard.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import { WalletIcon, WalletIconProps } from './WalletIcon';
 
 type WalletOptionCardProps = {
@@ -12,46 +14,52 @@ export const WalletOptionCard = ({
   title,
   description,
   icons,
-}: WalletOptionCardProps) => (
-  <button
-    type="button"
-    onClick={onClick}
-    style={{
-      width: '100%',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-      padding: 16,
-      borderRadius: 12,
-      color: '#F3F3F5',
-      backgroundColor: '#252525',
-      cursor: 'pointer',
-      textAlign: 'left',
-    }}>
-    <div>
-      <div style={{ fontSize: 14, fontWeight: 500 }}>{title}</div>
+}: WalletOptionCardProps) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      style={{
+        width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        padding: 16,
+        borderRadius: 12,
+        color: '#F3F3F5',
+        backgroundColor: isHovered ? '#2F2F2F' : '#252525',
+        cursor: 'pointer',
+        textAlign: 'left',
+      }}>
+      <div>
+        <div style={{ fontSize: 14, fontWeight: 500 }}>{title}</div>
+        <div
+          style={{
+            fontSize: 12,
+            fontWeight: 500,
+            color: '#B2B5C1',
+            marginTop: 8,
+            maxWidth: 170,
+          }}>
+          {description}
+        </div>
+      </div>
       <div
         style={{
-          fontSize: 12,
-          fontWeight: 500,
-          color: '#B2B5C1',
-          marginTop: 8,
-          maxWidth: 170,
+          display: 'grid',
+          gridTemplateColumns: '1fr 1fr',
+          gap: 6,
+          flexShrink: 0,
+          marginLeft: 16,
         }}>
-        {description}
+        {icons.map((iconProps, i) => (
+          <WalletIcon key={i} {...iconProps} />
+        ))}
       </div>
-    </div>
-    <div
-      style={{
-        display: 'grid',
-        gridTemplateColumns: '1fr 1fr',
-        gap: 6,
-        flexShrink: 0,
-        marginLeft: 16,
-      }}>
-      {icons.map((iconProps, i) => (
-        <WalletIcon key={i} {...iconProps} />
-      ))}
-    </div>
-  </button>
-);
+    </button>
+  );
+};
