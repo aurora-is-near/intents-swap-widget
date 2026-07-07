f126a2a17ffacb17091634875f2aa25c44f45e4f
Vashchuk Maksim <vashchukmaksim@gmail.com>
2026-07-07 14:31:01 +0200

    fix: increase max fee to 5 percent

--- apps/widget-creator/src/features/IntegrationModal/Fees/FeeInput.tsx
+++ apps/widget-creator/src/features/IntegrationModal/Fees/FeeInput.tsx
@@ -38,7 +38,7 @@ export const FeeInput = ({
             mask: Number,
             scale: 2, // max 2 decimal places
             min: 0, // minimum value
-            max: 1, // maximum value (1.00)
+            max: 5, // maximum value (1.00)
             radix: '.', // decimal separator
             mapToRadix: ['.'], // map dot key to radix
             padFractionalZeros: false, // don't force trailing zeros
